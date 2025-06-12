b72ea97226a561415df2eaf191e4c32995b59335
compusom <compusom@gmail.com>
2025-06-12 09:32:36 -0300

    Fix Top Ads bitacora header and formatting

---
+++ tests/test_formatting_utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from formatting_utils import safe_division, safe_division_pct
+
+def test_safe_division_scalar():
+    assert safe_division(10, 2) == 5
+    assert np.isnan(safe_division(1, 0))
+
+def test_safe_division_series():
+    result = safe_division(pd.Series([1, 2]), pd.Series([2, 0]))
+    assert result.tolist() == [0.5, np.nan]
+
+def test_safe_division_pct():
+    assert safe_division_pct(1, 2) == 50.0
+
